2e63901e165369f665c6e7690f796488b431ce83
AttentiveColon <bononcinid@gmail.com>
2023-02-18 18:52:29 -0800

    started on gltf loader

---
+++ bluebook/Chapter5/Load_GLTF_Mesh.cpp
@@ -0,0 +1,75 @@
+#include "Defines.h"
+#ifdef LOAD_GLTF_MESH
+#include "System.h"
+#include "tiny_gltf.h"
+#include <String>
+
+using namespace tinygltf;
+
+
+
+struct Application : public Program {
+	float m_clear_color[4];
+	u64 m_fps;
+	f64 m_time;
+
+	Application()
+		:m_clear_color{ 0.0f, 0.0f, 0.0f, 1.0f },
+		m_fps(0),
+		m_time(0)
+	{}
+
+	void OnInit(Input& input, Audio& audio, Window& window) {
+		audio.PlayOneShot("./resources/startup.mp3");
+
+		Model model;
+		TinyGLTF loader;
+		std::string err;
+		std::string warn;
+
+		bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, "./resources/two_planes.glb");
+
+		if (!warn.empty()) {
+			printf("Warn: %s\n", warn.c_str());
+		}
+
+		if (!err.empty()) {
+			printf("Err: %s\n", err.c_str());
+		}
+
+		if (!ret) {
+			printf("Failed to parse glTF\n");
+			assert(false);
+		}
+	}
+	void OnUpdate(Input& input, Audio& audio, Window& window, f64 dt) {
+		m_fps = window.GetFPS();
+		m_time = window.GetTime();
+	}
+	void OnDraw() {
+		static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+		glClearBufferfv(GL_COLOR, 0, m_clear_color);
+	}
+	void OnGui() {
+		ImGui::Begin("User Defined Settings");
+		ImGui::Text("FPS: %d", m_fps);
+		ImGui::Text("Time: %f", m_time);
+		ImGui::ColorEdit4("Clear Color", m_clear_color);
+		ImGui::End();
+	}
+};
+
+SystemConf config = {
+		1600,					//width
+		900,					//height
+		300,					//Position x
+		200,					//Position y
+		"Application",			//window title
+		false,					//windowed fullscreen
+		false,					//vsync
+		144,					//framelimit
+		"resources/Icon.bmp"	//icon path
+};
+
+MAIN(config)
+#endif //TEST
