b9719f99d3bfd5c2f322a7fe9ff01e00ea88aca7
AttentiveColon <bononcinid@gmail.com>
2023-05-05 16:45:05 -0700

    Prep for chapter 11 projects

---
+++ bluebook/Chapter11/High_Quality_Filtering.cpp
@@ -0,0 +1,96 @@
+#include "Defines.h"
+#ifdef HIGH_QUALITY_FILTERING
+#include "System.h"
+#include "Texture.h"
+#include "Model.h"
+#include "Mesh.h"
+
+static const GLchar* default_vertex_shader_source = R"(
+#version 450 core
+
+void main()
+{
+	
+}
+)";
+
+static const GLchar* default_fragment_shader_source = R"(
+#version 450 core
+
+void main()
+{
+
+}
+)";
+
+static ShaderText default_shader_text[] = {
+	{GL_VERTEX_SHADER, default_vertex_shader_source, NULL},
+	{GL_FRAGMENT_SHADER, default_fragment_shader_source, NULL},
+	{GL_NONE, NULL, NULL}
+};
+
+static const GLchar* compute_shader_source = R"(
+#version 450 core
+
+layout (local_size_x = 912) in;
+
+void main()
+{
+
+}
+)";
+
+static ShaderText compute_shader_text[] = {
+	{GL_COMPUTE_SHADER, compute_shader_source, NULL},
+	{GL_NONE, NULL, NULL}
+};
+
+struct Application : public Program {
+	float m_clear_color[4];
+	u64 m_fps;
+	f64 m_time;
+
+	GLuint m_vao;
+	GLuint m_program, m_compute_shader;
+
+	Application()
+		:m_clear_color{ 0.1f, 0.1f, 0.1f, 1.0f },
+		m_fps(0),
+		m_time(0.0)
+	{}
+
+	void OnInit(Input& input, Audio& audio, Window& window) {
+
+	}
+	void OnUpdate(Input& input, Audio& audio, Window& window, f64 dt) {
+		m_fps = window.GetFPS();
+		m_time = window.GetTime();
+	}
+	void OnDraw() {
+		static const GLfloat one = 1.0f;
+		glClearBufferfv(GL_COLOR, 0, m_clear_color);
+		glClearBufferfv(GL_DEPTH, 0, &one);
+	}
+	void OnGui() {
+		ImGui::Begin("User Defined Settings");
+		ImGui::Text("FPS: %d", m_fps);
+		ImGui::Text("Time: %f", m_time);
+		ImGui::ColorEdit4("Clear Color", m_clear_color);
+		ImGui::End();
+	}
+};
+
+SystemConf config = {
+		1600,					//width
+		900,					//height
+		300,					//Position x
+		200,					//Position y
+		"Application",			//window title
+		false,					//windowed fullscreen
+		false,					//vsync
+		144,					//framelimit
+		"resources/Icon.bmp"	//icon path
+};
+
+MAIN(config)
+#endif //HIGH_QUALITY_FILTERING
