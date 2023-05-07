72f95860558e05bab4b0058634d4990a4307bc3d
AttentiveColon <bononcinid@gmail.com>
2023-05-06 20:34:45 -0700

    Transform feedback example finished

--- bluebook/Chapter12/Transform_Feedback_Queries.cpp
+++ bluebook/Chapter12/Transform_Feedback_Queries.cpp
@@ -5,53 +5,20 @@
 #include "Model.h"
 #include "Mesh.h"
 
-static const GLchar* default_vertex_shader_source = R"(
+static const GLchar* transform_feedback_vertex_shader_source = R"(
 #version 450 core
 
-layout (location = 0)
-in vec3 position;
-layout (location = 1)
-in vec3 normal;
-layout (location = 2)
-in vec2 uv;
-
-layout (location = 4)
-uniform mat4 u_viewProj;
-
-out VS_OUT
-{
-	vec3 normal;
-	vec2 uv;
-} vs_out;
+out vec3 vPosition;
 
 void main(void)
 {
-    gl_Position = u_viewProj * vec4(position, 1.0);
-	vs_out.normal = normal;
-	vs_out.uv = uv;
+    const vec3 positions[] = vec3[] (vec3(-0.5, -0.5, 0.0), vec3(0.0, 0.5, 0.0), vec3(0.5, -0.5, 0.0));
+	vPosition = positions[gl_VertexID % 3];
 }
 )";
 
-static const GLchar* default_fragment_shader_source = R"(
-#version 450 core
-
-in VS_OUT
-{
-	vec3 normal;
-	vec2 uv;
-} fs_in;
-
-out vec4 color;
-
-void main()
-{
-	color = vec4(fs_in.uv.x, fs_in.uv.y, 0.0, 1.0);
-}
-)";
-
-static ShaderText default_shader_text[] = {
-	{GL_VERTEX_SHADER, default_vertex_shader_source, NULL},
-	{GL_FRAGMENT_SHADER, default_fragment_shader_source, NULL},
+static ShaderText transform_feedback_shader_text[] = {
+	{GL_VERTEX_SHADER, transform_feedback_vertex_shader_source, NULL},
 	{GL_NONE, NULL, NULL}
 };
 
@@ -60,17 +27,10 @@ struct Application : public Program {
 	u64 m_fps;
 	f64 m_time;
 
-	GLuint m_program, m_occlusion_program;
-	GLuint m_ubo;
-
+	GLuint m_transform_feedback_program;
 	GLuint m_queries;
-
-	ObjMesh m_cube;
-
-	SB::Camera m_camera;
-	bool m_input_mode = false;
-
-	Random m_random;
+	GLuint m_feedback_buffer;
+	GLuint m_result;
 
 	Application()
 		:m_clear_color{ 0.1f, 0.1f, 0.1f, 1.0f },
@@ -79,43 +39,59 @@ struct Application : public Program {
 	{}
 
 	void OnInit(Input& input, Audio& audio, Window& window) {
-		m_program = LoadShaders(default_shader_text);
-		m_camera = SB::Camera("Camera", glm::vec3(0.0f, 2.0f, -12.0f), glm::vec3(0.0f), SB::CameraType::Perspective, 16.0 / 9.0, 0.9, 0.01, 1000.0);
-		m_cube.Load_OBJ("./resources/cube.obj");
-		m_random.Init();
-
 		glGenQueries(1, &m_queries);
+
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		//Load our shader that will read data into our transform feedback buffer
+		//Declare the names of all the varyings we will copy data into
+		//Let Opengl know what are varyings are and how we want to pack them into buffers
+		//Relink the program
+		m_transform_feedback_program = LoadShaders(transform_feedback_shader_text);
+		static const char* varyings[] = { "vPosition" };
+		glTransformFeedbackVaryings(m_transform_feedback_program, 1, varyings, GL_INTERLEAVED_ATTRIBS);
+		glLinkProgram(m_transform_feedback_program);
+
+		//Generate a buffer to put our transform feedback data into, make it big enough to be able to hold all our data
+		glGenBuffers(1, &m_feedback_buffer);
+		glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, m_feedback_buffer);
+		glBufferData(GL_TRANSFORM_FEEDBACK_BUFFER, sizeof(float) * 3000, nullptr, GL_DYNAMIC_COPY);
 	}
 	void OnUpdate(Input& input, Audio& audio, Window& window, f64 dt) {
 		m_fps = window.GetFPS();
 		m_time = window.GetTime();
-
-		if (m_input_mode) {
-			m_camera.OnUpdate(input, 3.0f, 0.2f, dt);
-		}
-
-		//Implement Camera Movement Functions
-		if (input.Pressed(GLFW_KEY_LEFT_CONTROL)) {
-			m_input_mode = !m_input_mode;
-			input.SetRawMouseMode(window.GetHandle(), m_input_mode);
-		}
 	}
 	void OnDraw() {
 		static const GLfloat one = 1.0f;
 		glClearBufferfv(GL_COLOR, 0, m_clear_color);
 		glClearBufferfv(GL_DEPTH, 0, &one);
 
-		glEnable(GL_DEPTH_TEST);
 
-		glUseProgram(m_program);
-		glUniformMatrix4fv(4, 1, GL_FALSE, glm::value_ptr(m_camera.ViewProj()));
-		m_cube.OnDraw();
+		//Bind our program, bind out feedback buffer, begin our query of how many primitives are written to the transform feedback buffer
+		//Start transform feedback and start drawing
+		//End transform feedback, end our query
+		glUseProgram(m_transform_feedback_program);
+		glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, m_feedback_buffer);
+		glBeginQuery(GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN, m_queries);
+		glBeginTransformFeedback(GL_TRIANGLES);
+		glDrawArrays(GL_TRIANGLES, 0, 303);
+		glEndTransformFeedback();
+		glEndQuery(GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN);
+
+		//Call glFinish just to make sure our data is ready for the example
+		glFinish();
+
+		//Get our result to find out how many primitives were written into our transform feedback buffer
+		glGetQueryObjectuiv(m_queries, GL_QUERY_RESULT, &m_result);
 	}
 	void OnGui() {
 		ImGui::Begin("User Defined Settings");
 		ImGui::Text("FPS: %d", m_fps);
 		ImGui::Text("Time: %f", m_time);
 		ImGui::ColorEdit4("Clear Color", m_clear_color);
+		ImGui::Text("Primitives written: %d", m_result);
 		ImGui::End();
 	}
 };
