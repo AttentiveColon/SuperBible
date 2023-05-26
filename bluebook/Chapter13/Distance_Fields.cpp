76526470c50f98c5e4b574878d9e91f833b6e62c
AttentiveColon <bononcinid@gmail.com>
2023-05-26 12:24:03 -0700

    Distance field example

--- bluebook/Chapter13/Distance_Fields.cpp
+++ bluebook/Chapter13/Distance_Fields.cpp
@@ -6,27 +6,43 @@
 #include "Mesh.h"
 
 static const GLchar* vertex_shader_source = R"(
-#version 450 core
+#version 430 core
+
+out vec3 uv;
+
+layout (location = 0) uniform mat3 uv_transform;
+layout (location = 1) uniform mat4 mvp_matrix;
 
 void main(void)
 {
-    vec4 vertices[4] = vec4[4](vec4(-1.0, -1.0, 0.5, 1.0),
-                       vec4( 1.0, -1.0, 0.5, 1.0),
-                       vec4(-1.0,  1.0, 0.5, 1.0),
-                       vec4( 1.0,  1.0, 0.5, 1.0));
+    const vec2 pos[] = vec2[](vec2(-1.0, -1.0),
+                              vec2(1.0, -1.0),
+                              vec2(-1.0, 1.0),
+                              vec2(1.0, 1.0));
 
-    gl_Position = vertices[gl_VertexID];
+    vec3 uv3 = uv_transform * vec3( (pos[gl_VertexID] + vec2(1.0, 1.0)) * vec2(0.5, -0.5), 1.0);
+    uv.xy = uv3.xy / uv3.z;
+    uv.z = float(gl_InstanceID);
+    gl_Position = mvp_matrix * vec4(pos[gl_VertexID] + vec2(gl_InstanceID * 2, 0.0), 0.0, 1.0);
 }
 )";
 
 static const GLchar* fragment_shader_source = R"(
-#version 450 core
+#version 430 core
 
-out vec4 color;
+layout (location = 0) out vec4 color;
 
-void main()
+layout (binding = 0) uniform sampler2D sdf_texture;
+
+in vec3 uv;
+
+void main(void)
 {
-	color = vec4(1.0);
+    const vec4 c1 = vec4(0.1, 0.1, 0.2, 1.0);
+    const vec4 c2 = vec4(0.8, 0.9, 1.0, 1.0);
+    float val = texture(sdf_texture, vec2(uv.xy)).x;
+
+    color = mix(c1, c2, smoothstep(0.52, 0.48, val));
 }
 )";
 
@@ -44,6 +60,7 @@ struct Application : public Program {
 	f64 m_time;
 
 	GLuint m_program;
+	GLuint m_logo_texture;
 
 	SB::Camera m_camera;
 	bool m_input_mode = false;
@@ -57,6 +74,7 @@ struct Application : public Program {
 	void OnInit(Input& input, Audio& audio, Window& window) {
 		m_program = LoadShaders(shader_text);
 		m_camera = SB::Camera("Camera", glm::vec3(0.0f, 8.0f, 15.5f), glm::vec3(0.0f, 2.0f, 0.0f), SB::CameraType::Perspective, 16.0 / 9.0, 0.9, 0.01, 1000.0);
+		m_logo_texture = Load_KTX("./resources/gllogodistsm.ktx");
 
 		GLuint vao;
 		glGenVertexArrays(1, &vao);
@@ -81,6 +99,9 @@ struct Application : public Program {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(m_program);
+		glBindTextureUnit(0, m_logo_texture);
+		glUniformMatrix3fv(0, 1, GL_FALSE, glm::value_ptr(glm::mat3(1.0f)));
+		glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(m_camera.ViewProj()));
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	}
 	void OnGui() {
